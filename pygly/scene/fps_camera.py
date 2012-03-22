ba4e9b976633ab92d7865f3b8eb51a1a55483949
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-03-23 00:15:16 +1100

    Update fps_camera and six_dof_camera
    Add translate_forward and translate_backward
    to fps_camera.
    Added commenting to all functions.

--- pygly/scene/fps_camera.py
+++ pygly/scene/fps_camera.py
@@ -22,22 +22,37 @@ class FPS_Camera( object ):
         self.yaw = 0.0
     
     def set_orientation( self, pitch = None, yaw = None ):
+        """
+        Over-ride the current orientation.
+        Automatically calls update at the end.
+        TODO: add roll
+        """
         if pitch != None:
             self.pitch = pitch
         if yaw != None:
             self.yaw = yaw
         
-        self.update()
+        self._update()
     
     def orient( self, pitch = None, yaw = None ):
+        """
+        Add to the current orientation.
+        Automatically calls update at the end.
+        TODO: add roll
+        """
         if pitch != None:
             self.pitch += pitch
         if yaw != None:
             self.yaw += yaw
         
-        self.update()
+        self._update()
     
-    def update( self ):
+    def _update( self ):
+        """
+        Updates the orientation of the object.
+        This should be called once all of the yaw / pitch
+        modifications are applied.
+        """
         # limit our pitch to +- 1/2 pi
         if self.pitch > self.piOver2:
             self.pitch = self.piOver2
@@ -52,12 +67,24 @@ class FPS_Camera( object ):
         self.camera.orientation = quat
     
     def translate_object_forward( self, amount ):
+        """
+        Moves the object forward relative to
+        it's own orientation.
+        """
         self.camera.translate_forward( amount )
     
     def translate_object_backward( self, amount ):
+        """
+        Moves the object backward relative to
+        it's own orientation.
+        """
         self.camera.translateBackward( amount )
     
     def translate_inertial_forward( self, amount ):
+        """
+        Moves the object forward along the X,Z
+        plane ignoring the objects orientation.
+        """
         vec = [ 0.0, 0.0, -1.0 ]
         rotationQuat = maths.quaternion.set_to_rotation_about_y( self.yaw )
         
@@ -68,6 +95,10 @@ class FPS_Camera( object ):
         self.camera.translate( forwardVec )
     
     def translate_inertial_backward( self, amount ):
+        """
+        Moves the object backward along the X,Z
+        plane ignoring the objects orientation.
+        """
         vec = [ 0.0, 0.0, 1.0 ]
         rotationQuat = maths.quaternion.set_to_rotation_about_y( self.yaw )
         
@@ -78,20 +109,60 @@ class FPS_Camera( object ):
         self.camera.translate( backwardVec )
     
     def translate_inertial_up( self, amount ):
+        """
+        Moves the object up the Z axis
+        ignoring the objects orientation.
+        """
         self.camera.translate( [ 0.0, amount, 0.0 ] )
     
     def translate_inertial_down( self, amount ):
+        """
+        Moves the object down the Z axis
+        ignoring the objects orientation.
+        """
         self.camera.translate( [ 0.0, -amount, 0.0 ] )
     
     def translate_object_up( self, amount ):
+        """
+        Moves the object upward relative to
+        it's own orientation.
+        """
         self.camera.translate_up( amount )
     
     def translate_object_down( self, amount ):
+        """
+        Moves the object downward relative to
+        it's own orientation.
+        """
         self.camera.translate_down( amount )
+
+    def translate_forward( self, amount ):
+        """
+        The same as translate_inertial_forward.
+        Moves the object forward along the X,Z
+        plane.
+        """
+        self.translate_inertial_forward( amount )
+
+    def translate_backward( self, amount ):
+        """
+        The same as translate_inertial_backward.
+        Moves the object backward along the X,Z
+        plane.
+        """
+        self.translate_inertial_backward( amount )
     
     def translate_left( self, amount ):
+        """
+        In FPS camera, theres is no difference
+        between intertial left, and object left.
+        """
         self.camera.translate_left( amount )
     
     def translate_right( self, amount ):
+        """
+        In FPS camera, theres is no difference
+        between intertial right, and object right.
+        """
         self.camera.translate_right( amount )
     
--- pygly/scene/six_dof_camera.py
+++ pygly/scene/six_dof_camera.py
@@ -19,6 +19,9 @@ class SixDOF_Camera( object ):
         self.camera = None
     
     def orient( self, pitch = None, yaw = None, roll = None ):
+        """
+        Over-ride the current orientation.
+        """
         if yaw != None:
             quat = maths.quaternion.set_to_rotation_about_y( yaw )
             maths.quaternion.cross_product(
@@ -26,9 +29,12 @@ class SixDOF_Camera( object ):
                 self.camera.orientation,
                 out = self.camera.orientation
                 )
-            # mark the camera as dirty because the
-            # out= above doesn't trigger the property set method
-            self.camera._setDirty()
+            # we MUST manually set the node as dirty
+            # when we update the quaternion in
+            # place, the 'setter' function is not
+            # called and the node's 'dirty' flag does
+            # not get set
+            self.camera._set_dirty()
         if pitch != None:
             quat = maths.quaternion.set_to_rotation_about_x( pitch )
             maths.quaternion.cross_product(
@@ -36,9 +42,12 @@ class SixDOF_Camera( object ):
                 self.camera.orientation,
                 out = self.camera.orientation
                 )
-            # mark the camera as dirty because the
-            # out= above doesn't trigger the property set method
-            self.camera._setDirty()
+            # we MUST manually set the node as dirty
+            # when we update the quaternion in
+            # place, the 'setter' function is not
+            # called and the node's 'dirty' flag does
+            # not get set
+            self.camera._set_dirty()
         if roll != None:
             quat = maths.quaternion.set_to_rotation_about_z( roll )
             maths.quaternion.cross_product(
@@ -46,25 +55,52 @@ class SixDOF_Camera( object ):
                 self.camera.orientation,
                 out = self.camera.orientation
                 )
-            # mark the camera as dirty because the
-            # out= above doesn't trigger the property set method
-            self.camera._setDirty()
+            # we MUST manually set the node as dirty
+            # when we update the quaternion in
+            # place, the 'setter' function is not
+            # called and the node's 'dirty' flag does
+            # not get set
+            self.camera._set_dirty()
     
     def translate_forward( self, amount ):
+        """
+        Translates the object forward
+        based upon it's current orientation.
+        """
         self.camera.translate_forward( amount )
     
     def translate_backward( self, amount ):
-        self.camera.translatebackward( amount )
+        """
+        Translates the object backward
+        based upon it's current orientation.
+        """
+        self.camera.translate_backward( amount )
     
     def translate_up( self, amount ):
-        self.camera.translate_ip( amount )
+        """
+        Translates the object up
+        based upon it's current orientation.
+        """
+        self.camera.translate_up( amount )
     
     def translate_down( self, amount ):
+        """
+        Translates the object down
+        based upon it's current orientation.
+        """
         self.camera.translate_down( amount )
     
     def translate_left( self, amount ):
+        """
+        Translates the object left
+        based upon it's current orientation.
+        """
         self.camera.translate_left( amount )
     
     def translate_right( self, amount ):
+        """
+        Translates the object right
+        based upon it's current orientation.
+        """
         self.camera.translate_right( amount )
     
