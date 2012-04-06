b6ae643c6ee7d5cecdc8922f3f1738a95923d1b6
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-04-07 05:47:38 +1000

    Use winder.render for cocos layer
    The window.render call and cocos code now match.

--- pygly/cocos2d/layer.py
+++ pygly/cocos2d/layer.py
@@ -2,6 +2,9 @@
 Created on 23/03/2012
 
 @author: adam
+
+TODO: the viewport will need to handle cocos
+translations of the layer itself
 '''
 
 from cocos.layer.base_layers import Layer as CocosLayer
@@ -9,6 +12,7 @@ from cocos.director import director
 from pyglet.gl import *
 
 from renderer.viewport import Viewport
+import renderer.window as window
 
 
 class Layer( CocosLayer ):
@@ -22,8 +26,6 @@ class Layer( CocosLayer ):
 
         # create a default viewport that
         # stretches the entire screen
-        # TODO: this will need to handle cocos
-        # translations of the layer itself
         self.pygly_viewport = Viewport(
             [ 0.0, 0.0, 1.0, 1.0 ]
             )
@@ -43,38 +45,8 @@ class Layer( CocosLayer ):
         # enable depth testing
         director.set_depth_test( True )
 
-        # clear our depth buffer
-        self.pygly_viewport.clear(
+        window.render(
             director.window,
-            values = GL_DEPTH_BUFFER_BIT
+            [ self.pygly_viewport ]
             )
-
-        # apply our projection matrix
-        # cocos only sets the view matrix up once
-        # so we must store the existing view matrix
-        # so we can re-apply it at the end of our render
-        # store the current projection matrix
-        self.pygly_viewport.push_view_matrix()
-
-        # switch back to model view
-        # and store the current matrix so
-        # we can pop it off afterward
-        # apply the camera transforms
-        self.pygly_viewport.push_model_view()
-
-        # setup the viewport
-        self.pygly_viewport.setup_viewport()
-        self.pygly_viewport.render( director.window )
-        self.pygly_viewport.tear_down_viewport()
-
-        # pop our view matrix back to the
-        # original cocos matrix
-        self.pygly_viewport.pop_model_view()
-
-        # disable depth testing
-        director.set_depth_test( False )
-
-        # re-apply the original projection matrix
-        self.pygly_viewport.pop_view_matrix()
-        glMatrixMode(GL_MODELVIEW)
 
