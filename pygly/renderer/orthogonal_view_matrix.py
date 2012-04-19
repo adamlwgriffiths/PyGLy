f15cbdb388a06a032cedd6cb208c6ff940b1734d
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-04-19 12:07:29 +1000

    Change orthographic matrix values.
    Now from -half viewport ratio -> + halfviewport ratio.
    Add a TODO stating that this is temporary.

--- pygly/renderer/orthogonal_view_matrix.py
+++ pygly/renderer/orthogonal_view_matrix.py
@@ -2,13 +2,26 @@
 Created on 20/06/2011
 
 @author: adam
+
+TODO: create a "size" calculation tool.
+This would be like the controls available to
+setting a desktop background.
+This would receive a rectangle (the requested size)
+The viewport size in pixels
+And return a rectangle based on the following preferences:
+
+Original (just use size X for the orth size)
+Fit biggest (scale until the biggest dimension fits the screen)
+Fit smallest (scale until the smallest dimension fits the screen)
 '''
 
 import math
 
 from pyglet.gl import *
+import numpy
 
 from view_matrix import ViewMatrix
+from pygly.maths import rectangle
 
 
 class OrthogonalViewMatrix( ViewMatrix ):
@@ -19,6 +32,14 @@ class OrthogonalViewMatrix( ViewMatrix ):
         near_clip = 1.0,
         far_clip = 100.0
         ):
+        """
+        Initialises an Orthographic (2D) view matrix.
+
+        @param scale: The scale to applie to the matrix.
+        The dimensions of the view matrix are based on the viewport
+        size in order to keep the view ratio correct.
+        A fullscreen viewport 
+        """
         super( OrthogonalViewMatrix, self ).__init__()
 
         if far_clip <= near_clip:
@@ -26,6 +47,7 @@ class OrthogonalViewMatrix( ViewMatrix ):
 
         self.near_clip = near_clip
         self.far_clip = far_clip
+        self.scale = numpy.array( scale, dtype = numpy.float )
 
     def push_view_matrix( self, window, viewport ):
         # setup our projection matrix
@@ -33,15 +55,15 @@ class OrthogonalViewMatrix( ViewMatrix ):
         glPushMatrix()
         glLoadIdentity()
 
-        # set the ortho matrix to be from
-        # 0 -> width and 0 -> height
-        # with near clip of -1 and far clip
-        # of +1
+        # set the ortho matrix
         # http://stackoverflow.com/questions/4269079/mixing-2d-and-3d-in-opengl-using-pyglet
+        half_width = viewport.ratio_width / 2.0
+        half_height = viewport.ratio_height / 2.0
         glOrtho(
-            0, viewport.ratio_width,
-            0, viewport.ratio_height,
-            -1.0, 1.0
+            self.rect
+            -half_width, half_width,
+            -half_height, half_height,
+            self.near_clip, self.far_clip
             )
 
     def pop_view_matrix( self ):
