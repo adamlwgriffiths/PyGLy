e16ad4174b1689c5507a19a170137ab5458fe46d
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-09 09:05:41 +1000

    Remove version from submodule __init__.py files

--- pygly/input/__init__.py
+++ pygly/input/__init__.py
@@ -1,9 +1,6 @@
 import pkgutil
 import os
 
-# the version of software
-# this is used by the setup.py script
-__version__ = '20120508'
 
 # make 'from module import *' work dynamically.
 # otherwise we have to manually update the __all__ list.
