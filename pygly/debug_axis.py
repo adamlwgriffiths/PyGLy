fdb7c89bd27121fdf687356a1a07cd194a049af7
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-09 07:47:40 +1000

    Use pyglet labels for the debug_axis

--- pygly/debug_axis.py
+++ pygly/debug_axis.py
@@ -6,17 +6,18 @@ Created on 25/03/2012
 
 from pyglet.gl import *
 from pyglet.graphics import Batch
+from pyglet.text import Label
 
 
 display_list = None
+label_x = None
+label_y = None
+label_z = None
 
 def initialise():
     global display_list
     display_list = glGenLists( 1 )
     glNewList( display_list, GL_COMPILE )
-
-    # TODO: store existing line width value
-    glLineWidth( 1.0 )
 
     # draw the axis
     glBegin( GL_LINES )
@@ -35,54 +36,104 @@ def initialise():
 
     glEnd()
 
-    # draw the axis labels
-
-    # pyglet text labels are too blurry and we
-    # can't put them in display lists
-    # so we'll do some leet hax and draw our letters
-    # X label = \ + /
-    glBegin( GL_LINES )
-    # \
-    glVertex3f(-1.0 + 2.0, 1.0, 0.0 )
-    glVertex3f( 1.0 + 2.0,-1.0, 0.0 )
-    # /
-    glVertex3f(-1.0 + 2.0,-1.0, 0.0 )
-    glVertex3f( 1.0 + 2.0, 1.0, 0.0 )
-    glEnd()
-
-    # Y label = V + |
-    glBegin( GL_LINE_STRIP )
-    # V
-    glVertex3f(-1.0, 1.0 + 2.0, 0.0 )
-    glVertex3f( 0.0, 0.0 + 2.0, 0.0 )
-    glVertex3f( 1.0, 1.0 + 2.0, 0.0 )
-    glEnd()
-    # |
-    glBegin( GL_LINES )
-    glVertex3f( 0.0, 0.0 + 2.0, 0.0 )
-    glVertex3f( 0.0,-1.0 + 2.0, 0.0 )
-    glEnd()
-
-    # Z label = - + / + -
-    # we shall draw the z label along the Y/Z axis, not X/Y
-    # -
-    glBegin( GL_LINE_STRIP )
-    glVertex3f( 0.0, 1.0,-1.0 + 2.0 )
-    glVertex3f( 0.0, 1.0, 1.0 + 2.0 )
-    # /
-    glVertex3f( 0.0,-1.0,-1.0 + 2.0 )
-    # -
-    glVertex3f( 0.0,-1.0, 1.0 + 2.0 )
-    glEnd()
-
     glEndList()
+
+    # create some labels
+    global label_x
+    global label_y
+    global label_z
+
+    font = 'Times New Roman'
+    font_size = 18
+    dpi = 200.0
+
+    label_x = Label(
+        'X',
+        font_name = font,
+        font_size = font_size,
+        dpi = dpi,
+        anchor_x = 'center',
+        anchor_y = 'center'
+        )
+    label_y = Label(
+        'Y',
+        font_name = font,
+        font_size = font_size,
+        dpi = dpi,
+        anchor_x = 'center',
+        anchor_y = 'center'
+        )
+    label_z = Label(
+        'Z',
+        font_name = font,
+        font_size = font_size,
+        dpi = dpi,
+        anchor_x = 'center',
+        anchor_y = 'center'
+        )
 
 def render():
     global display_list
+    global label_x
+    global label_y
+    global label_z
 
     if display_list == None:
         initialise()
 
+    # we're going to mess around with GL state
+    # so push the existing values on the stack
+    glPushAttrib( GL_ALL_ATTRIB_BITS )
+    glPushMatrix()
+
+    # change the line width
+    glLineWidth( 3.0 )
+
     # render the axis
+    scale = 5.0
+    inv_scale = 1.0 / scale
+    glScalef( scale, scale, scale )
     glCallList( display_list )
+    glScalef( inv_scale, inv_scale, inv_scale )
+
+    # enable alpha blending
+    glEnable( GL_BLEND )
+    glBlendFunc( GL_SRC_ALPHA, GL_ONE )
+    glBlendEquation( GL_FUNC_ADD )
+
+    # disable depth buffer
+    # transparent objects still write to depth buffer
+    # so normally you would render solid geometry
+    # then back to front for transparent
+    # we don't want the transparent objects causing
+    # other objects to not render beheind them
+    glDepthMask( GL_FALSE )
+
+    translation = 5.0
+    scale = 0.1
+    inv_scale = 1.0 / scale
+
+    # draw X label
+    glTranslatef( translation, 0.0, 0.0 )
+    glScalef( scale, scale, scale )
+    label_x.draw()
+    glScalef( inv_scale, inv_scale, inv_scale )
+
+    # draw Y label
+    glTranslatef(-translation, translation, 0.0 )
+    glScalef( scale, scale, scale )
+    label_y.draw()
+    glScalef( inv_scale, inv_scale, inv_scale )
+
+    # draw Z label
+    glTranslatef( 0.0,-translation, translation )
+    # rotate down z axis
+    glRotatef( 90.0, 0.0, 1.0, 0.0 )
+    glScalef( scale, scale, scale )
+    label_z.draw()
+    glScalef( inv_scale, inv_scale, inv_scale )
+
+    # reset our gl state
+    glPopMatrix()
+    glPopAttrib()
 
