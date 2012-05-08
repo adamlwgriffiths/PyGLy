7c48e78c471057918a95401ed25f71dc4e18b456
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-05-08 13:12:31 +1000

    Move list tests into test_list.py

--- pygly/list.py
+++ pygly/list.py
@@ -15,24 +15,3 @@ def are_equivalent( a, b ):
 def not_equivalent( a, b ):
     return not are_equivalent( a, b )
 
-if __name__ == '__main__':
-    assert True == are_equivalent(
-        [1,2,3],
-        [1,2,3]
-        )
-    assert False == are_equivalent(
-        [1,2,3],
-        [1,3,2]
-        )
-    assert False == are_equivalent(
-        [1,2,3],
-        [4,5,6]
-        )
-    assert True == not_equivalent(
-        [-1,-1],
-        [ 0, 0]
-        )
-    assert False == not_equivalent(
-        [1,2,3],
-        [1,2,3]
-        )
