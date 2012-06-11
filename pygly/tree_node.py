1d32a6cc14857562c6056bfde99f7ae6cbccf85f
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-06-12 04:35:59 +1000

    Modify tree_node to use pyglet events
    We now send events for child added / removed
    and parent set

--- pygly/tree_node.py
+++ pygly/tree_node.py
@@ -7,8 +7,10 @@ Created on 11/05/2012
 import weakref
 from collections import deque
 
+from pyglet.event import EventDispatcher
+
     
-class TreeNode( object ):
+class TreeNode( EventDispatcher ):
     """
     Base class for Tree branch objects.
 
@@ -19,7 +21,7 @@ class TreeNode( object ):
         super( TreeNode, self ).__init__()
         
         self._parent = None
-        self.children = set()
+        self._children = set()
     
     def add_child( self, node ):
         """
@@ -32,10 +34,16 @@ class TreeNode( object ):
             raise ValueError( "Node has an existing parent" )
         
         # add the node
-        self.children.add( node )
+        self._children.add( node )
         
         # set ourself as the parent
-        node._parent = weakref.ref( self )
+        node.parent = self
+
+        # notify others of our change
+        self.dispatch_event(
+            'on_child_added',
+            node
+            )
     
     def remove_child( self, node ):
         """
@@ -45,10 +53,20 @@ class TreeNode( object ):
         is not a child of the node.
         """
         # remove from our list of children
-        self.children.remove( node )
+        self._children.remove( node )
 
         # unset the node's parent
-        node._parent = None
+        node.parent = None
+
+        # notify others of our change
+        self.dispatch_event(
+            'on_child_removed',
+            node
+            )
+
+    @property
+    def children( self ):
+        return self._children
     
     @property
     def parent( self ):
@@ -61,20 +79,45 @@ class TreeNode( object ):
             return self._parent()
         return None
 
+    @parent.setter
+    def parent( self, parent ):
+        """
+        Sets the parent of the node.
+        This should not be called manually.
+
+        @raise ValueError: Raised if the node
+        already has a parent or if the node is
+        not a child of the new parent.
+        """
+        if self.parent != None:
+            raise ValueError( "Node has an existing parent" )
+        if self not in parent.children:
+            raise ValueError( "Node not child of parent" )
+
+        old_parent = self.parent
+        self._parent = weakref.ref( parent )
+
+        # notify others of our change
+        self.dispatch_event(
+            'on_parent_changed',
+            old_parent,
+            self.parent
+            )
+
     def dfs( self ):
-        queue = deque( list(self.children) )
+        queue = deque( list(self._children) )
         while queue:
             node = queue.pop()
             if hasattr( node, 'children' ):
-                queue.extend( list(node.children) )
+                queue.extend( list(node._children) )
             yield node
 
     def bfs( self ):
-        queue = deque( list(self.children) )
+        queue = deque( list(self._children) )
         while queue:
             node = queue.pop()
             if hasattr( node, 'children' ):
-                queue.extendleft( list(node.children) )
+                queue.extendleft( list(node._children) )
             yield node
 
     def predecessors( self ):
@@ -82,4 +125,28 @@ class TreeNode( object ):
         while parent != None:
             yield parent
             parent = parent.parent
+
+    # document our events
+    if hasattr( sys, 'is_epydoc' ):
+        def on_parent_changed( old_parent, new_parent ):
+            '''A parent was set.
+
+            :event:
+            '''
+
+        def on_child_added( child ):
+            '''A child was added
+
+            :event:
+            '''
+
+        def on_child_removed( child ):
+            '''A child was removed
+
+            :event:
+            '''
+
+TreeNode.register_event_type( 'on_parent_changed' )
+TreeNode.register_event_type( 'on_child_added' )
+TreeNode.register_event_type( 'on_child_removed' )
 
