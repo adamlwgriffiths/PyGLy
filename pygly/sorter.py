29715f6bfeea73a52fb48eae4f5e382928902030
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-06-15 13:40:25 +1000

    Add radius sorting and rename existing to plane
    Radius sorting is better for transparency as
    objects beside one another don't cause issues.
    Probably best to use radius sort always.

--- pygly/sorter.py
+++ pygly/sorter.py
@@ -6,7 +6,9 @@ Created on 07/06/2012
 
 import numpy
 
-def sort_front_to_back(
+import pyrr
+
+def sort_plane_front_to_back(
     render_position,
     render_direction,
     objects,
@@ -19,9 +21,7 @@ def sort_front_to_back(
     relative_positions = object_positions - render_position
 
     # do a dot product against the render direction
-    dot_values = numpy.apply_along_axis(
-        numpy.dot,
-        relative_positions.ndim - 1,
+    dot_values = pyrr.vector.dot(
         relative_positions,
         render_direction
         )
@@ -38,14 +38,61 @@ def sort_front_to_back(
 
     return sorted_objects
 
-def sort_back_to_front(
+def sort_plane_back_to_front(
     render_position,
     render_direction,
     objects,
     object_positions
     ):
     # sort front to back
-    sorted_objects = sort_front_to_back(
+    sorted_objects = sort_plane_front_to_back(
+        render_position,
+        render_direction,
+        objects,
+        object_positions
+        )
+
+    # reverse the array
+    return sorted_objects[::-1]
+
+def sort_radius_front_to_back(
+    render_position,
+    render_direction,
+    objects,
+    object_positions
+    ):
+    if len( object_positions ) <= 0:
+        return numpy.empty( 0 )
+
+    # make positions relative to render position
+    relative_positions = object_positions - render_position
+
+    # do a dot product against the render direction
+    lengths = pyrr.vector.squared_length(
+        relative_positions
+        )
+
+    # sort the object array by the
+    # dot product of the object positions
+    # sort in ascending order
+
+    # this first call sorts and returns the indices
+    inds = numpy.argsort( lengths, axis = -1 )
+
+    # we then rebuilt the object array by the indices
+    sorted_objects = numpy.take( objects, inds )
+
+    # return inverted
+    return sorted_objects
+
+def sort_radius_back_to_front(
+    render_position,
+    render_direction,
+    objects,
+    object_positions
+    ):
+    # sort front to back
+    sorted_objects = sort_radius_front_to_back(
         render_position,
         render_direction,
         objects,
