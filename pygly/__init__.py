9b5c2fc2f3e70b39455dd5924468ba6105a53082
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-10-02 17:40:47 +1000

    Make __init__ set __all__ rather than import

--- pygly/__init__.py
+++ pygly/__init__.py
@@ -8,6 +8,8 @@ from version import __version__
 # make 'from module import *' work dynamically.
 # otherwise we have to manually update the __all__ list.
 # http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+__all__ = []
+
 for module in os.listdir( os.path.dirname( __file__ ) ):
     name, extension = os.path.splitext( module )
 
@@ -24,7 +26,5 @@ for module in os.listdir( os.path.dirname( __file__ ) ):
     if extension not in extensions: 
         continue
 
-    # file type is ok, import it
-    __import__( name, locals(), globals() )
-    del module
+    __all__.append( name )
 
