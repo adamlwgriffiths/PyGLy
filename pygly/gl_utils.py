8af4f304684b235dbaea7b640467ca324465a519
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-03-01 18:26:41 +1100

    Add numpy_utils and move function from gl_utils
    Move numpy_to_gl_enum to numpy_utils.
    Add many functions for processing numpy dtypes.

--- pygly/gl_utils.py
+++ pygly/gl_utils.py
@@ -4,33 +4,19 @@ to both Core and Legacy profiles.
 .. moduleauthor:: Adam Griffiths <adam.lw.griffiths@gmail.com>
 """
 
-from OpenGL.GL import *
+from OpenGL import GL
 
 def string_to_type( type ):
-    return globals()[type]
+    return getattr( GL, type )
 
 def type_to_string( glType ):
     return {
-        GLbyte:     "GLbyte",
-        GLubyte:    "GLubyte",
-        GLshort:    "GLshort",
-        GLushort:   "GLushort",
-        GLint:      "GLint",
-        GLuint:     "GLuint",
-        GLfloat:    "GLfloat",
-        GLdouble:   "GLdouble",
+        GL.GLbyte:     "GLbyte",
+        GL.GLubyte:    "GLubyte",
+        GL.GLshort:    "GLshort",
+        GL.GLushort:   "GLushort",
+        GL.GLint:      "GLint",
+        GL.GLuint:     "GLuint",
+        GL.GLfloat:    "GLfloat",
+        GL.GLdouble:   "GLdouble",
         }[ glType ]
-
-def numpy_dtype_to_enum( array ):
-    return {
-        'int8':     GL_BYTE,
-        'uint8':    GL_UNSIGNED_BYTE,
-        'int16':    GL_SHORT,
-        'uint16':   GL_UNSIGNED_SHORT,
-        'int32':    GL_INT,
-        'uint32':   GL_UNSIGNED_INT,
-        'float16':  GL_HALF_FLOAT,
-        'float32':  GL_FLOAT,
-        'float64':  GL_DOUBLE,
-        }[ array.dtype ]
-
---
+++ pygly/numpy_utils.py
@@ -0,0 +1,179 @@
+"""Provides utilities for processing numpy arrays.
+"""
+
+import numpy
+
+
+def dtype_stride( dtype, name = None ):
+    """Returns the number of bytes in a single row of the
+    specified dtype.
+    """
+    if name:
+        return dtype[ name ].itemsize
+    else:
+        return dtype.itemsize
+
+def dtype_offset( dtype, name = None ):
+    """Returns the initial offset of the named property.
+
+    If no name is given, 0 is returned.
+    """
+    if name:
+        # get the dtype for the named value
+        # the offset is the second value
+        return dtype.fields[ name ][ 1 ]
+    else:
+        return 0
+
+def dtype_element_count( dtype, name = None ):
+    """Returns the number of values that compose a dtype.
+
+    If you need the number of elements of a specifically named
+    column, pass the root dtype in with the name.
+    Ie.
+
+    type = dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    num_elements( type, 'position' )
+    >>> 3
+    type
+    >>> dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    type.descr
+    >>> [('position', '<f4', (3,)), ('colour', '<f4', (3,))]
+
+    Do NOT pass a subdtype or the count will be wrong.
+    Ie.
+
+    numpy.dtype( [
+        ('position', 'f4', (3,)).
+        ('colour', 'i2', (3,))
+        ])
+    num_elements( dtype[ 'position' ] )
+    >>> 1
+    type[ 'position' ]
+    >>> dtype(('float32',(3,)))
+    type[ 'position' ].descr
+    >>> [('', '|V12')]
+    """
+    def count( descr ):
+        if len( descr ) > 2:
+            shape = descr[ 2 ]
+            # multiply the shape
+            return reduce( lambda x, y: x * y, shape )
+        else:
+            return 1
+
+    descr = dtype.descr
+
+    # we've been given a name
+    if name:
+        # find the name in the descriptions
+        for property in descr:
+            if property[ 0 ] == name:
+                return count( property )
+
+        # name not found
+        raise ValueError( "Property not found" )
+    else:
+        # no name given
+        # sum the number of values for each dtype
+        sum = 0
+        for type in descr:
+            sum += count( type )
+        return sum
+
+def dtype_type( dtype, name = None ):
+    """Returns the type that compose a dtype.
+
+    If you need the type of a specifically named
+    column, pass the root dtype in with the name.
+    Ie.
+
+    type = dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    extract_type( type, 'position' )
+    >>> '<f4'
+    type
+    >>> dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    type.descr
+    >>> [('position', '<f4', (3,)), ('colour', '<f4', (3,))]
+
+    Do NOT pass a subdtype or the count will be wrong.
+    Ie.
+
+    numpy.dtype( [
+        ('position', 'f4', (3,)).
+        ('colour', 'i2', (3,))
+        ])
+    num_elements( dtype[ 'position' ] )
+    >>> '|V12'
+    type[ 'position' ]
+    >>> dtype(('float32',(3,)))
+    type[ 'position' ].descr
+    >>> [('', '|V12')]
+    """
+    if name:
+        for property in dtype.descr:
+            if property[ 0 ] == name:
+                return property[ 1 ]
+        raise ValueError( "Property not found" )
+    else:
+        if len( dtype.descr ) > 1:
+            raise ValueError( "Multiple types present" )
+
+        return dtype.descr[ 0 ][ 1 ]
+
+def dtype_gl_enum( dtype, name = None ):
+    """Returns the type that compose a dtype.
+
+    If you need the type of a specifically named
+    column, pass the root dtype in with the name.
+    Ie.
+
+    type = dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    gl_enum( type, 'position' )
+    >>> GL_FLOAT
+    type
+    >>> dtype([('position', '<f4', (3,)), ('colour', '<f4', (3,))])
+    type.descr
+    >>> [('position', '<f4', (3,)), ('colour', '<f4', (3,))]
+
+    Do NOT pass a subdtype or the count will be wrong.
+    Ie.
+
+    numpy.dtype( [
+        ('position', 'f4', (3,)).
+        ('colour', 'i2', (3,))
+        ])
+    gl_enum( dtype[ 'position' ] )
+    >>> KeyError: 'V12'
+    type[ 'position' ]
+    >>> dtype(('float32',(3,)))
+    type[ 'position' ].descr
+    >>> [('', '|V12')]
+    """
+    from OpenGL import GL
+    import re
+
+    # remove endian identifiers
+    # http://docs.scipy.org/doc/numpy/reference/generated/numpy.dtype.byteorder.html#numpy.dtype.byteorder
+    type = dtype_type( dtype, name )
+    stripped_type = re.sub('[=<>|]', '', type)
+
+    return {
+        'int8':     GL.GL_BYTE,
+        'i1':       GL.GL_BYTE,
+        'uint8':    GL.GL_UNSIGNED_BYTE,
+        'u1':       GL.GL_UNSIGNED_BYTE,
+        'int16':    GL.GL_SHORT,
+        'i2':       GL.GL_SHORT,
+        'uint16':   GL.GL_UNSIGNED_SHORT,
+        'u2':       GL.GL_UNSIGNED_SHORT,
+        'int32':    GL.GL_INT,
+        'i4':       GL.GL_INT,
+        'uint32':   GL.GL_UNSIGNED_INT,
+        'u4':       GL.GL_UNSIGNED_INT,
+        'float32':  GL.GL_FLOAT,
+        'f4':       GL.GL_FLOAT,
+        'float64':  GL.GL_DOUBLE,
+        'f8':       GL.GL_DOUBLE,
+        }[ stripped_type ]
+
