a33e17fb22e5463d1182df036af16504c9c59123
Adam Griffiths <adam.lw.griffiths@gmail.com>
2013-03-02 14:27:07 +1100

    Use the new TypedBuffer legacy support.

--- pygly/examples/legacy/cube.py
+++ pygly/examples/legacy/cube.py
@@ -1,11 +1,11 @@
 import numpy
 from OpenGL.GL import *
-from OpenGL.arrays.vbo import VBO
-from OpenGL.GL.ARB.vertex_array_object import *
+
+from pygly.buffer import TypedBuffer, BufferRegion
 
 vbo = None
 
-vertices = numpy.array([
+vertices_simple = numpy.array([
      1.0, 1.0,-1.0,     0.0, 1.0, 0.0,
     -1.0, 1.0,-1.0,     0.0, 1.0, 0.0,
      1.0, 1.0, 1.0,     0.0, 1.0, 0.0,
@@ -51,38 +51,45 @@ vertices = numpy.array([
      dtype = 'float32'
      )
 
-
+# convert to our named view
+vertices = vertices_simple.view(
+    dtype = [
+        ('position',    'float32',  (3,)),
+        ('colour',      'float32',  (3,))
+        ]
+    )
 
 def create():
     # TODO: convert this to a display list
     global vbo
     global vertices
 
-    vbo = glGenBuffers( 1 )
-    glBindBuffer( GL_ARRAY_BUFFER, vbo )
-    glBufferData(
+    vbo = TypedBuffer(
         GL_ARRAY_BUFFER,
-        vertices.nbytes,
-        vertices,
-        GL_STATIC_DRAW
+        GL_STATIC_DRAW,
+        (vertices.size, vertices.dtype)
         )
+    print vbo
 
-    # unbind our buffers
-    glBindBuffer( GL_ARRAY_BUFFER, 0 )
+    vertices_buffer = vbo[ 0 ]
+
+    vbo.bind()
+    vertices_buffer.set_data( vertices )
+    vbo.unbind()
 
 def draw():
     global vbo
     global vertices
 
-    glBindBuffer( GL_ARRAY_BUFFER, vbo )
-    # vertices
-    glEnableClientState( GL_VERTEX_ARRAY )
-    glVertexPointer( 3, GL_FLOAT, 6 * 4, None )
-    # colours
-    glEnableClientState( GL_COLOR_ARRAY )
-    glColorPointer( 3, GL_FLOAT, 6 * 4, ctypes.c_void_p(3 * 4) )
+    vbo.bind()
 
-    glDrawArrays( GL_TRIANGLES, 0, vertices.size / 3 )
+    # get the vertices buffer
+    vertices_buffer = vbo[ 0 ]
 
-    glBindBuffer( GL_ARRAY_BUFFER, 0 )
+    vertices_buffer.vertex_pointer( 'position' )
+    vertices_buffer.color_pointer( 'colour' )
+
+    glDrawArrays( GL_TRIANGLES, 0, vertices.size )
+
+    vbo.unbind()
 
